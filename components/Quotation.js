229f1f3672e9938a8b3f6218e81c4ac70d81e930
KarollG <42561606+KarollG@users.noreply.github.com>
2021-09-27 03:07:37 -0500

    cambios en estilos cotiz

--- components/Quotation.js
+++ components/Quotation.js
@@ -2,10 +2,41 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 const Quotation = ({result}) => {
-  if(Object.keys(result).length === 0 ) return null;
+  if (Object.keys(result).length === 0) return null;
   return (
-    <Text>El precios{result.PRICE}</Text>
-    );
+    <View style={styles.result}>
+      <Text style={styles.text}>
+        <Text style={styles.span}>{result.PRICE}</Text>
+      </Text>
+      <Text style={styles.text}> Precio más alto del día
+        <Text style={styles.span}>{result.PRICE}</Text>
+      </Text>
+      <Text style={styles.text}>
+        <Text style={styles.span}>{result.PRICE}</Text>
+      </Text>
+      <Text style={styles.text}>
+        <Text style={styles.span}>{result.PRICE}</Text>
+      </Text>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  result: {
+
+  },
+
+  text: {
+
+  },
+
+  price: {
+
+  },
+
+  span: {
+
+  },
+});
 
 export default Quotation;
